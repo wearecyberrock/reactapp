ed08a304f50f2c1023e013937ea0eeffcc12fbfa
wearecyberrock <wearecyberrock@gmail.com>
2026-06-18 21:07:44 -0500

    float

--- src/components/parallaxtest.tsx
+++ src/components/parallaxtest.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+﻿import React, { useState } from 'react';
 import { motion, Variants } from "framer-motion";
 import SumfestPopup from './SumfestPopup'; // Explicit
 const sectionVariants: Variants = {
